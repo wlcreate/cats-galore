b4100075183bedf0baf880250f87bd6e15cf655c
isabel k. lee <hello@isabelklee.co>
2020-05-18 22:41:32 -0400

    refactoring like functionality

--- src/CatTile.jsx
+++ src/CatTile.jsx
@@ -2,42 +2,49 @@ import React, { Component } from 'react'
 
 class CatTile extends Component {
   state = {
-    showTile: false,
+    showDetails: false,
     likes: 0
   }
 
   toggleTile = (event) => {
-    this.setState((prevState) => {
-      return {
-        showTile: !prevState.showTile
-      }
+    this.setState({
+        showDetails: !this.state.showDetails
     })
   }
 
   handleLikes = (event) => {
-    event.preventDefault() 
+    event.preventDefault()
+
     let newLikes = this.props.cat.likes + 1
-    console.log(newLikes)
-    this.setState({
+
+    fetch(`http://localhost:3000/cats/${this.props.cat.id}`, {
+      method: "PATCH",
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify({
         likes: newLikes
+      })
     })
+    .then(r => r.json())
+    .then(console.log)
   }
 
   render() {
-    let { name, age, sex, picture, likes } = this.props.cat
+    let { name, age, sex, picture, likes, ["favorite toy"]: toy } = this.props.cat
 
       return(
         <div className="cat-tile">
-          <img alt={ `${name} cat` } src={ picture }/>
+          <img alt={ `${name} cat` } src={ picture } onClick = { this.toggleTile } />
           <h3>{ name }</h3>
-          <button onClick={ this.toggleTile }>About {name}</button>
+          <button onClick={ this.handleLikes }>Likes: { likes } </button>
 
-          { this.state.showTile ?
+          { this.state.showDetails ?
           <>
             <p>Age: { age }</p>
             <p>Sex: { sex }</p>
-            <p>Likes: { likes }</p>
-            <button onClick={ this.handleLikes }>Like</button>
+            <p>Favorite toy: { toy }</p>
           </>
           : null }
         </div>    
